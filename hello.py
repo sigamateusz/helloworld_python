e49b3dbdbbdd92d071c49a8bb8e9f5154a221404
Mateusz Siga <sigamateusz@gmail.com>
2016-10-27 17:17:45 +0200

    first commit

---
+++ hello.py
@@ -0,0 +1,6 @@
+import sys
+print("Hello",end=" ") # print "Hello" and adds a space instead of the new line
+if len(sys.argv)>1: # checks whether the received user name
+    print(sys.argv[1].title()+"!") # if true, print user name with uppercased
+else:                              # first letter and adds "!"
+    print("World!") # if false, print "World!"
