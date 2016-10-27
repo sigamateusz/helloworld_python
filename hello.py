18ade0489bfb0562495282b10d199d79b4490490
Mateusz Siga <sigamateusz@gmail.com>
2016-10-27 19:39:50 +0200

    Add clear screen

--- hello.py
+++ hello.py
@@ -1,4 +1,7 @@
 import sys
+import os
+os.system('clear')
+
 print("Hello",end=" ") # print "Hello" and adds a space instead of the new line
 if len(sys.argv)>1: # checks whether the received user name
     print(sys.argv[1].title()+"!") # if true, print user name with uppercased
